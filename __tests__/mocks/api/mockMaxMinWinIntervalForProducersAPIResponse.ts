2687457e21374810a1c1fc0daba9b32104e2523f
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-03-01 21:01:35 -0300

    adding tests mocks

---
+++ __tests__/mocks/api/mockMaxMinWinIntervalForProducersAPIResponse.ts
@@ -0,0 +1,20 @@
+import { type IIntervalList } from '@/types'
+
+export const mockCityAPIResponse: IIntervalList = {
+  min: [
+    {
+      producer: 'Joel Silver',
+      interval: 1,
+      previousWin: 1990,
+      followingWin: 1991,
+    },
+  ],
+  max: [
+    {
+      producer: 'Matthew Vaughn',
+      interval: 13,
+      previousWin: 2002,
+      followingWin: 2015,
+    },
+  ],
+}
--- src/services/getMaxMinWinIntervalForProducers.test.ts
+++ src/services/getMaxMinWinIntervalForProducers.test.ts
@@ -1,31 +1,13 @@
+import { mockCityAPIResponse } from '../../__tests__/mocks/api/mockMaxMinWinIntervalForProducersAPIResponse'
 import { api } from './api'
 import { getMaxMinWinIntervalForProducers } from './getMaxMinWinIntervalForProducers'
 
 describe('API: getMaxMinWinIntervalForProducers', () => {
   it('should return maximun and minimun interval for producers', async () => {
-    const data = {
-      min: [
-        {
-          producer: 'Joel Silver',
-          interval: 1,
-          previousWin: 1990,
-          followingWin: 1991,
-        },
-      ],
-      max: [
-        {
-          producer: 'Matthew Vaughn',
-          interval: 13,
-          previousWin: 2002,
-          followingWin: 2015,
-        },
-      ],
-    }
-
     jest.spyOn(api, 'get').mockResolvedValue({
-      data,
+      data: mockCityAPIResponse,
     })
     const response = await getMaxMinWinIntervalForProducers()
-    expect(response).toBe(data)
+    expect(response).toBe(mockCityAPIResponse)
   })
 })
