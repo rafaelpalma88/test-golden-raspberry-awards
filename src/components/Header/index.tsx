5a6a0f6d63e57fb664a4861a7bd81560d9108d70
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-03-24 10:59:07 -0300

    changing tailwind order css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,12 +6,12 @@ export function Header(): JSX.Element {
     <>
       <Menubar className="px-7">
         <MenubarMenu>
-          <MenubarTrigger className="py-2 px-3 outline-none select-none leading-none rounded text-violet11 text-[13px] flex items-center justify-between gap-[2px] data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 ">
+          <MenubarTrigger className="text-violet11 data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 flex select-none items-center justify-between gap-[2px] rounded px-3 py-2 text-[13px] leading-none outline-none ">
             <Link href="/list">Movies List</Link>
           </MenubarTrigger>
         </MenubarMenu>
         <MenubarMenu>
-          <MenubarTrigger className="py-2 px-3 outline-none select-none leading-none rounded text-violet11 text-[13px] flex items-center justify-between gap-[2px] data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4">
+          <MenubarTrigger className="text-violet11 data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 flex select-none items-center justify-between gap-[2px] rounded px-3 py-2 text-[13px] leading-none outline-none">
             <Link href="/dashboard">Dashboard</Link>
           </MenubarTrigger>
         </MenubarMenu>
